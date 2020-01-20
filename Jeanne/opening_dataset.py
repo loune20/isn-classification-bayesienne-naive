ee6ee562d6f230ac42a4569ec3ff6cca0c6f4d63
loune20 <loumercier@protonmail.com>
2020-01-20 23:12:56 +0000

    Delete opening_dataset.py

--- Jeanne/opening_dataset.py
+++
@@ -1,9 +0,0 @@
-import csv #importing csv python library
-      
-reader = csv.DictReader(open('data2.csv')) #opening file
-        
-list_data = [] #creating empty list
-for row in reader: #for each row in our csv file
-    list_data.append((row['summary'], row['overall'])) #adding at the end of the list a tuple of the summary and the rating extracted from the csv file
-    
-print(list_data) #print our final result
