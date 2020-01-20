e7355e56160584a9a22c1dfa7f5b8e78b9af2329
Jeanne D <dpy.jeanne@gmail.com>
2020-01-20 18:35:03 +0000

    Update Test.py

--- Jeanne/Test.py
+++ Jeanne/Test.py
@@ -1,4 +1,8 @@
 import csv
-reader = csv.DictReader(open('Dataset_30reviews.csv'))
+reader = csv.DictReader(open('Dataset_30reviews.csv')) #ouvrir fichier csv
+
+# dico = [dict(row) for row in reader] #creation d'une liste de dictionnaires
+# print (dico)
+
 for row in reader:
-    print(row['reviews.rating'], row['reviews.text'])
+   print(row['reviews.rating'], row['reviews.text']) #afficher une ou plusieurs colonnes
