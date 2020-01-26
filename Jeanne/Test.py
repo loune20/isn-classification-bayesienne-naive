f47bb5b8cf79f420f9dd191fd5640263438551b4
Jeanne D <dpy.jeanne@gmail.com>
2020-01-26 20:37:48 +0000

    Update Jeanne/Test.py

--- Jeanne/Test.py
+++ Jeanne/Test.py
@@ -1,3 +1,5 @@
+#ouvrir CSV :
+
 import csv
 reader = csv.DictReader(open('Dataset_30reviews.csv')) #ouvrir fichier csv
 
@@ -6,3 +8,24 @@ reader = csv.DictReader(open('Dataset_30reviews.csv')) #ouvrir fichier csv
 
 for row in reader:
    print(row['reviews.rating'], row['reviews.text']) #afficher une ou plusieurs colonnes
+
+
+
+
+# compter mots et frequence :
+
+number = {}
+freq_word = {}
+
+com = "oui oui non mais pas oui or non"
+word_list = com.split() #creation d'une liste en separant chaque mot par un espace
+
+for mot in word_list :
+    number[mot] = word_list.count(mot) 
+
+print ("nombre de mots :", number)
+
+for mot in number :
+    freq_word[mot] = word_list.count(mot)/len(number)
+    
+print ("frequence :", freq_word)
