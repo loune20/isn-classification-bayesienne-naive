55b8a818446245c91e272b289494b293c6c169f4
Jeanne D <dpy.jeanne@gmail.com>
2020-01-27 20:52:29 +0000

    Update Jeanne/Test.py

--- Jeanne/Test.py
+++ Jeanne/Test.py
@@ -11,21 +11,37 @@ for row in reader:
 
 
 
-
+#____________________________________________________________________________________________
 # compter mots et frequence :
 
-number = {}
+number_of_words = {}
 freq_word = {}
+words_list = []
+word_counter = {}
 
-com = "oui oui non mais pas oui or non"
-word_list = com.split() #creation d'une liste en separant chaque mot par un espace
+data_set = (("first com",True), ("com com com2",False), ("com3",True), ("com test",True))
+number_of_com = len(data_set)
 
-for mot in word_list :
-    number[mot] = word_list.count(mot) 
-
-print ("nombre de mots :", number)
-
-for mot in number :
-    freq_word[mot] = word_list.count(mot)/len(number)
+for i in range (0, number_of_com) : #create a list of all the words 
+    com = data_set[i][0] 
+    words_list = words_list + com.split() #add words separated by a space to the list
     
+print (words_list)
+
+for word in words_list : 
+    number_of_words[word] = words_list.count(word) #count number of each word in all comments
+    
+for word in number_of_words :
+    word_counter[word] = 0 #create a dict with all the words as keys
+
+#A CORRIGER : compte le nb total de mots et pas uniquement nb de coms avec mot :
+for word in word_counter :
+    for i in range(0, number_of_com) :
+        if word in data_set[i][0] :
+            word_counter[word] = word_counter[word] + 1
+
+    freq_word[word] = word_counter[word]/number_of_com #word frequency in comments
+
+print ("word_counter :", word_counter)
+print ("nombre de mots :", number_of_words)
 print ("frequence :", freq_word)
