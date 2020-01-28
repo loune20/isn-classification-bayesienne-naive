df09e7f1ba0ef286af99b9df3da2c142e7a9e07e
Nolkyr <nolkyr@protonmail.com>
2020-01-28 10:46:28 +0000

    Update test.py

--- Mathis/test.py
+++ Mathis/test.py
@@ -1,7 +1,9 @@
 def preProcessing(data_original):    
     my_list = []
+    data_treated = []
     for i in range(len(data_original)): #Populate empty list
-        my_list.append(None) 
+        my_list.append(None)
+        data_treated.append(None)
     for i in range(len(data_original)):
         my_list[i] = data_original[i][0]
     common = ['ourselves', 'hers', 'between', 'yourself', 'but', 'again', 'there', 'about', 'once', 'during', 'out', 'very', 'having', 'with', 'they', 'own', 'an', 'be', 'some', 'for', 'do', 'its', 'yours', 'such', 'into', 'of', 'most', 'itself', 'other', 'off', 'is', 's', 'am', 'or', 'who', 'as', 'from', 'him', 'each', 'the', 'themselves', 'until', 'below', 'are', 'we', 'these', 'your', 'his', 'through', 'don', 'nor', 'me', 'were', 'her', 'more', 'himself', 'this', 'down', 'should', 'our', 'their', 'while', 'above', 'both', 'up', 'to', 'ours', 'had', 'she', 'all', 'no', 'when', 'at', 'any', 'before', 'them', 'same', 'and', 'been', 'have', 'in', 'will', 'on', 'does', 'yourselves', 'then', 'that', 'because', 'what', 'over', 'why', 'so', 'can', 'did', 'not', 'now', 'under', 'he', 'you', 'herself', 'has', 'just', 'where', 'too', 'only', 'myself', 'which', 'those', 'i', 'after', 'few', 'whom', 't', 'being', 'if', 'theirs', 'my', 'against', 'a', 'by', 'doing', 'it', 'how', 'further', 'was', 'here', 'than']
@@ -19,11 +21,12 @@ def preProcessing(data_original):
             del(my_list[i][index[l]])
         index = []
         my_list[i]=" ".join(my_list[i])
+        data_treated[i] = [(my_list[i],data_original[i][1] )]
+    return(data_treated)
 
-    return(my_list)
 
-
-#To-do : remettre forme data_original
 
 data_original = [("ourselves are here in between the monkey is great", True), ("once upon a time the wizard was very happy it is a good news", False), ("blabla is a very good friend", True)]
 data_treated = preProcessing(data_original)
+print(data_original)
+print(data_treated)
