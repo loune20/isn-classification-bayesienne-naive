5fd2a58c365a246141bdbc01884314ac6d7ac06b
Nolkyr <nolkyr@protonmail.com>
2020-01-28 08:36:36 +0000

    Update test.py

--- Mathis/test.py
+++ Mathis/test.py
@@ -1,9 +1,18 @@
-my_list = [' A an elephant of the Biggest World','a','AZfydyTeZA']
-common = [' a ',' an ',' the ',' of ',' I ',' you ']
-
+my_list = ['A an elephant of the Biggest World','a','AZfydyTeZA']
+common = ['ourselves', 'hers', 'between', 'yourself', 'but', 'again', 'there', 'about', 'once', 'during', 'out', 'very', 'having', 'with', 'they', 'own', 'an', 'be', 'some', 'for', 'do', 'its', 'yours', 'such', 'into', 'of', 'most', 'itself', 'other', 'off', 'is', 's', 'am', 'or', 'who', 'as', 'from', 'him', 'each', 'the', 'themselves', 'until', 'below', 'are', 'we', 'these', 'your', 'his', 'through', 'don', 'nor', 'me', 'were', 'her', 'more', 'himself', 'this', 'down', 'should', 'our', 'their', 'while', 'above', 'both', 'up', 'to', 'ours', 'had', 'she', 'all', 'no', 'when', 'at', 'any', 'before', 'them', 'same', 'and', 'been', 'have', 'in', 'will', 'on', 'does', 'yourselves', 'then', 'that', 'because', 'what', 'over', 'why', 'so', 'can', 'did', 'not', 'now', 'under', 'he', 'you', 'herself', 'has', 'just', 'where', 'too', 'only', 'myself', 'which', 'those', 'i', 'after', 'few', 'whom', 't', 'being', 'if', 'theirs', 'my', 'against', 'a', 'by', 'doing', 'it', 'how', 'further', 'was', 'here', 'than']
+index = []
 for i in range(len(my_list)):
     my_list[i] = my_list[i].lower()#put all in lowercase
-    for j in range(len(common)):
-        if common[j] in (my_list[i]):#find common word
-            my_list[i] = my_list[i].replace(common[j], ' ')#and delete it
+    my_list[i] = my_list[i].split()
+    for j in range(len(my_list[i])):
+        for k in range(len(common)):
+            if my_list[i][j] == common[k] :#find common
+                index.append(j)
+                index.sort()
+                index.reverse()
+    for l in range(len(index)):
+        del(my_list[i][index[l]])
+    index = []
+    my_list[i]=" ".join(my_list[i])
 print(my_list)
+print(index)
