1a7a5706b5628c4492e88bc26ba8f1ced8c47bdb
loune20 <loumercier@protonmail.com>
2020-02-11 07:20:55 +0000

    Duplicates gérés

--- Louise/score_positivite/counting-words.py
+++ Louise/score_positivite/counting-words.py
@@ -1,5 +1,8 @@
-data_treated = [['word1',False], ['word2', True], ['word2', True], ['word3', True]]
+data_treated = [['word1',False], ['word2', True], ['word2', True], ['word3', True], ['word2', True]]
 
 words_list = []   
-for i in range (len(data_treated)) :
+for i in range (len(data_treated)):
     words_list = words_list + data_treated[i][0].split() #list of all words (after pre-processing, in all comments)
+for word in words_list:
+    if words_list.count(word) > 1:
+        words_list.remove(word)
