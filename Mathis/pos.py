0102554ea20424ea635df98b56a2f70cfea92c2f
Nolkyr <nolkyr@protonmail.com>
2020-02-11 08:20:27 +0000

    Add new file

---
+++ Mathis/pos.py
@@ -0,0 +1,21 @@
+def frequencyOfWordsInPos (data_set): #function that calculates the frequency of each word in all comments
+    comments_list_of_pos = [] #list with the (pre-processed) text of each com positive
+    comments_pos =0
+    freq_word = {}
+    word_counter = {} #dictionary with the word and the number of comments positive containing the word
+    
+    for i in range(len(data_set)):
+        if data_set[i][1] == True :
+            comments_list_of_pos.append(data_set[i][0])
+            comments_pos += 1
+    for word in words_list:
+        word_counter[word] = 0 #create a dictionary with all the words as keys, and 0 as value
+        for i in range (comments_pos):
+            if word in comments_list_of_pos[i]: #count number of comments positive containing the word
+                word_counter[word] += 1 
+        freq_word[word] = word_counter[word]/len(data_set) #frequency of comments positive containing the word
+        
+        if freq_word[word] <= 0.1:
+            del freq_word[word] #delete words with a frequency <= 0.1 in the list freq_word
+
+    return (freq_word)
